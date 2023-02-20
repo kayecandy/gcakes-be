8d3f52557086f1f57e4fbc8b2cd21e1ba564a691
Candice Canoso <candice.canoso@dtn.com>
2023-02-21 01:36:07 +0800

    Added AddProduct endpoint

--- src/contentful.ts
+++ src/contentful.ts
@@ -1,19 +1,44 @@
-import fetch, { BodyInit } from 'node-fetch';
+import fetch, { BodyInit, RequestInfo, RequestInit } from "node-fetch";
 
 export const ContentfulConfig = {
   space: process.env.CONTENTFUL_SPACE ?? "",
-  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN ?? "",
-  graphQlUrl: `${process.env.CONTENTFUL_GRAPHQL_URL}/spaces/${process.env.CONTENTFUL_SPACE}`,
+  contentDelivery: {
+    accessToken: process.env.CONTENTFUL_CD_ACCESS_TOKEN ?? "",
+    graphQlUrl: `${process.env.CONTENTFUL_GRAPHQL_URL}/spaces/${process.env.CONTENTFUL_SPACE}`,
+  },
+  contentManagement: {
+    accessToken: process.env.CONTENTFUL_CM_ACCESS_TOKEN ?? "",
+    url: `${process.env.CONTENTFUL_CM_URL}/spaces/${process.env.CONTENTFUL_SPACE}`,
+  },
 };
 
 export const fetchGQL = (body?: BodyInit) => {
-  return fetch(ContentfulConfig.graphQlUrl, {
+  return fetch(ContentfulConfig.contentDelivery.graphQlUrl, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
       Accept: "application/json",
-      Authorization: `Bearer ${ContentfulConfig.accessToken}`,
+      Authorization: `Bearer ${ContentfulConfig.contentDelivery.accessToken}`,
     },
     body,
+  });
+};
+
+/**
+ * A normal fetch() call with pre-attached header for
+ * Contentful CM Authorization header
+ *
+ * @param url - URL of request prepended with Contentful CM API URL
+ * @param init - Request configuration with attached header
+ */
+export const fetchCM = (url: RequestInfo, init?: RequestInit) => {
+  return fetch(`${ContentfulConfig.contentManagement.url}/${url}`, {
+    ...init,
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+      Authorization: `Bearer ${ContentfulConfig.contentManagement.accessToken}`,
+      ...init?.headers,
+    },
   });
 };
