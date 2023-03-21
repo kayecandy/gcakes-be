4735e118b018fe6fc4679c25cb33dc3fd487bd19
akn5 <104754302+alankun5@users.noreply.github.com>
2023-03-21 15:16:16 +0800

    [STSWENG-125] Add: View Product API endpoint

---
+++ api/products/view/[productId].ts
@@ -0,0 +1,41 @@
+import { StatusCodes } from 'http-status-codes';
+
+import {
+  VercelRequest,
+  VercelResponse,
+} from '@vercel/node';
+
+import { fetchGQL } from '../../../src/contentful';
+
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+        const t = await (
+          await fetchGQL(
+            JSON.stringify({
+              query: `
+                query ($productId: String) {
+                  productCollection(where: { sys: { id: $productId } }) {
+                    items {
+                      sys {
+                        id
+                      }
+                      name
+                      price
+                      description
+                      image {
+                        url
+                      }
+                      productType
+                    }
+                  }
+                }
+              `,
+              variables: {
+                productId: req.query.productId,
+              },
+            })
+          )
+        ).json()
+    
+        return res.status(StatusCodes.OK).json(t.data.productCollection.items);
+  }
+  
