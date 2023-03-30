ddcc43e7663e1cf75bc703f30063c4fc9ca6a83e
akn5 <104754302+alankun5@users.noreply.github.com>
2023-03-30 18:16:28 +0800

    [STSWENG-178] Fix: Add Orders API

--- api/orders/add.ts
+++ api/orders/add.ts
@@ -2,96 +2,93 @@ import { VercelRequest, VercelResponse } from "@vercel/node";
 import { StatusCodes } from "http-status-codes";
 import { fetchCM } from "../../src/contentful";
 import { MultiHandler, withMultiHandlers } from "../../src/handlers";
+import { AuthMiddlewareData } from "../../src/middlewares/auth-middleware";
+import { errorMiddleware } from "../../src/middlewares/error-middleware";
 import { methodMiddleware } from "../../src/middlewares/method-middleware";
 import { publishMiddleware } from "../../src/middlewares/publish-middleware";
 
-const addOrderHandler: MultiHandler = async(req, res) => {
-
-    const order = await (
-        await fetchCM("entries", {
-            method: "POST",
-            headers: {
-                "X-Contentful-Content-Type": "order",
-            },
-            body: JSON.stringify({
-                fields: {
-                    status: {
-                        "en-US": req.body.status,
-                    },
-                    date: {
-                        "en-US": req.body.date,
-                    },
-                    customer: {
-                        "en-US": {
-                            sys: {
-                                linkType: "Entry",
-                                id: req.body.userId,
-                            },
-                        },
-                    },
-                    product: {
-                        "en-US": {
-                            sys: {
-                                linkType: "Entry",
-                                id: req.body.productId
-                            },
-                        },
-                    },
-                    // productsCollection: {
-                    //     "en-US": {
-                    //         type: "Array",
-                    //         items: {
-                    //             sys: {
-                    //                 id: req.body.productIds,
-                    //                 linkType: "Entry",
-                    //             },
-                    //         },
-                    //     },
-                    // },
-                    quantity: {
-                        "en-US": req.body.quantity,
-                    },
-                    deliveryAddress: {
-                        "en-US": req.body.deliveryAddress,
-                    },
-                    // paymentMethod: {
-                    //     "en-US": req.body.paymentMethod,
-                    // },
+const addOrderHandler: MultiHandler = async(
+    req, 
+    res,
+    data: AuthMiddlewareData = {}
+    ) => {
+    try {
+        const order = await (
+            await fetchCM("entries", {
+                method: "POST",
+                headers: {
+                    "X-Contentful-Content-Type": "order",
                 },
-            }),
-        })
-    );
+                body: JSON.stringify({
+                    fields: {
+                        status: {
+                            "en-US": req.body.status,
+                        },
+                        date: {
+                            "en-US": req.body.date,
+                        },
+                        customer: {
+                            "en-US": {
+                                sys: {
+                                    linkType: "Entry",
+                                    id: req.body.userId,
+                                },
+                            },
+                        },
+                        products: {
+                            "en-US": [{
+                                sys: {
+                                    id: req.body.productId,
+                                    linkType: "Entry",
+                                },
+                            }],
+                        },
+                        quantity: {
+                            "en-US": req.body.quantity,
+                        },
+                        deliveryAddress: {
+                            "en-US": req.body.deliveryAddress,
+                        },
+                        // paymentMethod: {
+                        //     "en-US": req.body.paymentMethod,
+                        // },
+                    },
+                }),
+            })
+        );
 
-    if (!order.ok) {
-        throw order;
-    }
-    
-    const orderJson = await order.json();
-
-    if (req.body.published) {
-        await fetchCM(`entries/${orderJson.sys.id}/published`, {
-            method: "PUT",
-            headers: {
-                "X-Contentful-Version": "1",
-            }
-        });
-    }
-
-    console.log("Order request ", req.body);
-
-    return {
-        action: "next",
-        response: res,
-        data: {
-          entryId: orderJson.sys.id
+        if (!order.ok) {
+            throw order;
         }
-      };
 
-    //return res.status(StatusCodes.OK).json(order)
+        const orderJson = await order.json();
+
+        console.log("Order request ", req.body);
+
+        return {
+            action: "next",
+            response: res,
+            data: {
+            ...data,
+            entryId: orderJson.sys.id
+            }
+        };
+    } catch (e) {
+        return {
+          action: "next",
+          response: res,
+          data: {
+            ...data,
+            error: e,
+          },
+        };
+      }
+
 }
 
 export default withMultiHandlers([
     methodMiddleware(["POST"]),
     addOrderHandler,
-    publishMiddleware
+    publishMiddleware,
+    errorMiddleware
 ])
